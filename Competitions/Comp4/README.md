3475c32227badc3af263cff6770a609e653db4c4
Mandy Meindersma <meinders@ualberta.ca>
2018-03-28 14:46:50 -0600

    sounds work

--- Competitions/Comp4/README.md
+++ Competitions/Comp4/README.md
@@ -3,4 +3,4 @@ download:
 
 play a sound:
 * rosrun sound_play soundplay_node.py 
-* rosrun sound_play play.py /usr/mandy/home/winter18/Robotics/Competitions/Comp4/meow.ogg
+* rosrun sound_play play.py ~/winter18/Robotics/Competitions/Comp4/meow.ogg
---
+++ Competitions/Comp4/start_sound.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from sound_play.libsoundplay import SoundClient
+# from sound_play.msg import SoundRequest
+import rospy
+import time
+
+rospy.init_node('sound')
+
+soundthing = SoundClient()
+time.sleep(1)
+# soundthing.play(SoundRequest.NEEDS_UNPLUGGING)
+# soundthing.voiceSound("Testing the new A P I")
+soundthing.playWave("/home/mandy/winter18/Robotics/Competitions/Comp4/meow.ogg")
+print("sound started")
